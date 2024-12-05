07f0ec8bac6273501eb1aa603fff9f2170555e35
Felipe Oliveira <cvfelipeoliveira@gmail.com>
2024-12-05 11:09:37 -0500

    comiit

--- src/main/java/br/com/gempi/waze_api_client/service/WazeApiServiceImpl.java
+++ src/main/java/br/com/gempi/waze_api_client/service/WazeApiServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import br.com.gempi.generated.Alert;
